93ed873fd633ce0f5cde1f2a9b4a59075fab742c
nav-ios <131914355+nav-ios@users.noreply.github.com>
2026-09-22 20:49:37 -0400

    Add `EventsFeed` framework with `EventsFeedTests` target and shared `CI` scheme

---
+++ EventsFeed/Event.swift
@@ -0,0 +1,36 @@
+//
+//  Event.swift
+//  EventsFeed
+//
+//  Created by Navdeep Rana on 22/09/26.
+//
+
+import Foundation
+
+public struct Event: Equatable {
+    public let id: UUID
+    public let title: String
+    public let location: EventLocation
+    public let time: Date
+    public let imageURL: URL
+
+    public init(id: UUID, title: String, location: EventLocation, time: Date, imageURL: URL) {
+        self.id = id
+        self.title = title
+        self.location = location
+        self.time = time
+        self.imageURL = imageURL
+    }
+}
+
+public struct EventLocation: Equatable {
+    public let name: String
+    public let latitude: Double
+    public let longitude: Double
+
+    public init(name: String, latitude: Double, longitude: Double) {
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
