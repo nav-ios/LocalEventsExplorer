8764885b6f6af903e2f3ba078acb02b77c5bafc6
nav-ios <131914355+nav-ios@users.noreply.github.com>
2026-09-22 21:03:04 -0400

    Asserts that calling `load` twice requests data from the URL twice

--- EventsFeedTests/RemoteEventsLoaderTests.swift
+++ EventsFeedTests/RemoteEventsLoaderTests.swift
@@ -25,6 +25,16 @@ final class RemoteEventsLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url])
     }
 
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+
+        sut.load()
+        sut.load()
+
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+
     // MARK: - Helpers
 
     private func makeSUT(url: URL = URL(string: "https://any-url.com")!) -> (sut: RemoteEventsLoader, client: HTTPClientSpy) {
